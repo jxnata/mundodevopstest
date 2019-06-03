74cb25710f523a7ad3ebfd0d90e8f625daabd269
jxnata <jonata.95ol@gmail.com>
2019-06-03 19:18:09 -0300

    Finalização

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, ScrollView, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import ListFilters from '../components/ListFilters';
 import ListExercices from '../components/ListExercices';
 import Separator from '../components/Separator';
@@ -22,6 +22,7 @@ export default class Home extends Component {
 	{
         this.setState({data});
     }
+
     filter(tipo) {
         let array = this.state.checkeds;
         let index = array.indexOf(tipo)
@@ -34,11 +35,11 @@ export default class Home extends Component {
             <View style={styles.theme.container}>
                 <StatusBar backgroundColor={styles.colors.secondary} barStyle="light-content"/>
                 <Separator marginTop={10} marginBottom={10}/>
-                { this.state.data.filters.length > 0 &&(
+                {/* { this.state.data.filters.length > 0 &&( */}
                     <View style={styles.theme.box}>
                     <ListFilters filters={this.state.data.filters} checkeds={this.state.checkeds} onPress={param => this.filter(param)}/>
                 </View>
-                )}
+                {/* )} */}
                 <Separator marginBottom={10} line={false}/>
                 <ListExercices exercices={this.state.data.exercices}/>
             </View>
